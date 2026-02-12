a148331859c9484a4658e0d85c8b0aa383c53dac
tve24el030-svg <tve24el030@cet.ac.in>
2026-02-12 22:28:15 +0530

    code uploaded

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 
 public class AreaCalculator {
-    
-    // TODO: Define calculateArea(float radius) -> returns area of circle
-    // Use Math.PI or 3.14159f for pi
-
-    // TODO: Define calculateArea(float length, float breadth) -> returns area of rectangle
-
-    // TODO: Define calculateArea(double base, double height) -> returns area of triangle
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // TODO: Read input for Circle (radius)
-        // TODO: Read input for Rectangle (length, breadth)
-        // TODO: Read input for Triangle (base, height)
         
-        // TODO: Call methods and print results formatted to 2 decimal places
-        // Hint: Use System.out.printf("%.2f\n", area);
+        double r = sc.nextDouble();  // Radius
+        double l = sc.nextDouble();  // Length
+        double b = sc.nextDouble();  // Breadth
+        double ba = sc.nextDouble(); // Base
+        double h = sc.nextDouble();  // Height
+
+        double carea = Math.PI * r * r;
+        double rarea = l * b;
+        double tarea = 0.5 * ba * h;
+        System.out.printf("%.2f%n", carea);
+        System.out.printf("%.2f%n", rarea);
+        System.out.printf("%.2f%n", tarea);
+        
+        sc.close();
     }
 }
